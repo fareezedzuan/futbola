3fd46760bcede7157e2ba6b46e93afc62f706244
fareezedzuan <fareezedzuan@gmail.com>
2025-05-02 00:28:39 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,5 @@
-import { supabase } from './supabaseClient.js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+const supabase = createClient('https://kdbqroxhypnadolcxxxc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtkYnFyb3hoeXBuYWRvbGN4eHhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE3MjkwNzYsImV4cCI6MjA1NzMwNTA3Nn0.c7_RVxoFdJNqQ62R3t1emH2Wf4dSsQaunHsHmbQxOBA');
 
 // Google login
 document.getElementById("google-login").addEventListener("click", async () => {
