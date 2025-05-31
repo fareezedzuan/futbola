6de353813a41962710ab4f8f36b4849115f81c2c
fareezedzuan <fareezedzuan@gmail.com>
2025-06-01 01:15:28 +0800

    booking new

--- api/get-bookings.js
+++ api/get-bookings.js
@@ -26,6 +26,8 @@ async function handler(req, res) {
 
   try {
     const { data, error } = await supabase
+      // .from("bookings") 
+      // .select("id, name, phone, status")
       .from("game_bookings")
       .select("id, user_id, jersey_name, phone, avatar_url, booked_by_user_id")
       .eq("game_id", gameId);
